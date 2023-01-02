f0d8485871524e8524d14adf70a0b683c2c3b78c
DESKTOP-EUE29QT\tester <mearct@gmail.com>
2023-01-02 10:35:35 +0800

    simple sample

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome');
+Route::view('/contact-us', 'contact');
+Route::view('/about', 'about');
+
+Route::get('customers', function () {
+    
+    $customers = [
+        'John Doe',
+        'Jane Doe',
+        'Bob The Builder',
+    ];
+    
+    return view( 'internals.customers', [
+        'anothername' => $customers,
+    ]);
 });
