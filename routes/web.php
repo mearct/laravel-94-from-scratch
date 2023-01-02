53da67687ec615adf1a2d064291fc93f87f2bf7a
DESKTOP-EUE29QT\tester <mearct@gmail.com>
2023-01-02 11:42:58 +0800

    controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CustomersController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+Route::view('/contact-us', 'contact');
+Route::view('/about', 'about');
+Route::get('/customers', [CustomersController::class, 'list']);
+
