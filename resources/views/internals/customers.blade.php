4bc4927d66f9fabc81e70d992d3b1190c9f95285
DESKTOP-EUE29QT\tester <mearct@gmail.com>
2023-01-04 12:52:41 +0800

    adding email for customers

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -6,13 +6,28 @@
 
     <form action="customers" method="POST" class="pb-5">
 
-        <div class="input-group">
-            <input type="text" name="name">
+        <div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">Name</span>
+                <input type="text" name="name" value="{{ old( 'name' )}}" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+                {{ $errors -> first( 'name' ) }}
+            </div>
+        </div>
 
+        <div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">Email</span>
+                <input type="email" name="email" value="{{ old( 'email' )}}" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+                {{ $errors -> first( 'email' ) }}
+            </div>
         </div>
-        <div class="input-group">
-            {{ $errors -> first( 'name' ) }}
-        </div>
+
+
+
 
         <button type="submit">Add Customer</button>
         @csrf
@@ -20,7 +35,7 @@
 
     <ul>
         @foreach ( $customers as $customer )
-            <li> {{ $customer -> name }} </li>
+            <li> {{ $customer -> name }} <span class="text-muted">{{ $customer -> email }}</span></li>
         @endforeach
     </ul>
 @endsection
